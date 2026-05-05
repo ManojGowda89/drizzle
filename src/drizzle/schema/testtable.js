67b0d1d9fbd143f9db55069d377451490dc385e1
ManojGowda89 <mail@manojgowda.in>
2026-05-05 17:29:14 +0530

    first commit

---
+++ src/drizzle/schema/testtable.js
@@ -0,0 +1,11 @@
+import { boolean } from "drizzle-orm/gel-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  deviceid: varchar({ length: 255 }).notNull().unique(),
+
+});
